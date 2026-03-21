22b6dc7609b14c7af20f95448fe3ba13272ae31e
Sam Hensonn <ackerlite623@gmail.com>
2026-03-21 17:41:33 +0800

    21.03.26: Fix binding for Railway

--- server/auth.js
+++ server/auth.js
@@ -56,6 +56,6 @@ app.post('/betage_postreqver', (req, res) => {
 
 const PORT = process.env.PORT || 5050;
 
-app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
